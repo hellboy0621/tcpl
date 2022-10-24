2e13e8a56b7b59da239ea59c3a6f2c2cdc8cf478
hellboy0621 <hero0621@126.com>
2022-10-24 11:22:31 +0800

    feat: add ch05 p97 指针数组初始化之月份名称

---
+++ ch05/p97.c
@@ -0,0 +1,13 @@
+
+// 返回第 n 个月份的名字
+char *month_name(int n) {
+    static char *name[] = {
+        "Illegal month",
+        "January", "February", "March",
+        "April", "May", "June",
+        "July", "August", "September",
+        "October", "November", "December"
+    };
+
+    return (n < 1 || n > 12) ? name[0] : name[n];
+}
